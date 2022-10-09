6b1b93f11e406d5a36fa99d08751c915a9d51fba
Mlie <emipa606@users.noreply.github.com>
2022-10-09 21:37:58 +0200

    Mod updated for 1.4 and passed autotests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BadMeatCategory
 
-![Image](https://i.imgur.com/WAEzk68.png)
+![Image](https://i.imgur.com/buuPQel.png)
 
 
 Puts all meat that is defined as coming from a Humanlike race and would cause bad thoughts from eating in its own category 
@@ -9,24 +9,15 @@ Also moves meat from Insectoids, rotten meat from https://steamcommunity.com/sha
 Russian translation by Tkhakiro
 
 Idea by: Rasutei 
-
-[table]
-    [tr]
-        [td]https://invite.gg/Mlie]![Image](https://i.imgur.com/zdzzBrc.png)
-[/td]
-        [td]https://github.com/emipa606/BadMeatCategory]![Image](https://i.imgur.com/kTkpTOE.png)
-[/td]
-    [/tr]
-[/table]
 	
-![Image](https://i.imgur.com/pgjQLXV.png)
+![Image](https://i.imgur.com/O0IIlYj.png)
 
 Since modding is just a hobby for me I expect no donations to keep modding. If you still want to show your support you can gift me anything from my https://store.steampowered.com/wishlist/id/Mlie]Wishlist or buy me a cup of tea.
 
-https://ko-fi.com/G2G55DDYD]![Image](https://i.ibb.co/VWJJb3w/Support-Me-dark-2x.png)
+https://ko-fi.com/G2G55DDYD]![Image](https://i.imgur.com/Utx6OIH.png)
 
 
-![Image](https://i.imgur.com/Rs6T6cr.png)
+![Image](https://i.imgur.com/PwoNOj4.png)
 
 
 
@@ -36,6 +27,7 @@ https://ko-fi.com/G2G55DDYD]![Image](https://i.ibb.co/VWJJb3w/Support-Me-dark-2x
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
+
 
 
 
--- Source/BadMeatCategory/BadMeatCategory.cs
+++ Source/BadMeatCategory/BadMeatCategory.cs
@@ -2,84 +2,83 @@
 using RimWorld;
 using Verse;
 
-namespace BadMeatCategory
+namespace BadMeatCategory;
+
+[StaticConstructorOnStartup]
+public class BadMeatCategory
 {
-    [StaticConstructorOnStartup]
-    public class BadMeatCategory
+    static BadMeatCategory()
     {
-        static BadMeatCategory()
+        var MeatRawCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatRaw");
+        if (MeatRawCategory == null)
         {
-            var MeatRawCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatRaw");
-            if (MeatRawCategory == null)
-            {
-                Log.ErrorOnce("[BadMeatCategory]: Could not find the MeatRaw-category. Will not sort bad meat.",
-                    "MeatRawCategory".GetHashCode());
-                return;
-            }
-
-            var MeatBadCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatBad");
-            if (MeatBadCategory == null)
-            {
-                Log.ErrorOnce("[BadMeatCategory]: Could not find the MeatBad-category. Will not sort bad meat.",
-                    "MeatBadCategory".GetHashCode());
-                return;
-            }
-
-            var extraThingDefs = new List<string>
-            {
-                "Meat_Chaocow",
-                "MeatRotten"
-            };
-            var thingsToMove = new List<ThingDef>();
-            foreach (var descendantThingDef in MeatRawCategory.DescendantThingDefs)
-            {
-                if (FoodUtility.GetMeatSourceCategory(descendantThingDef) == MeatSourceCategory.Humanlike)
-                {
-                    thingsToMove.Add(descendantThingDef);
-                    continue;
-                }
-
-                if (descendantThingDef.ingestible.sourceDef?.race != null &&
-                    descendantThingDef.ingestible.sourceDef.race.FleshType == FleshTypeDefOf.Insectoid)
-                {
-                    thingsToMove.Add(descendantThingDef);
-                    continue;
-                }
-
-                if (extraThingDefs.Contains(descendantThingDef.defName))
-                {
-                    thingsToMove.Add(descendantThingDef);
-                }
-            }
-
-            foreach (var thingDef in thingsToMove)
-            {
-                MeatRawCategory.childThingDefs.Remove(thingDef);
-                thingDef.thingCategories.Remove(MeatRawCategory);
-                MeatBadCategory.childThingDefs.Add(thingDef);
-                thingDef.thingCategories.Add(MeatBadCategory);
-                if (thingDef.defName != "MeatRotten")
-                {
-                    continue;
-                }
-
-                // Support for Rotted Meat-mod
-                var rottenCategoryDef = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatRawRotten");
-                if (rottenCategoryDef == null)
-                {
-                    continue;
-                }
-
-                rottenCategoryDef.childThingDefs.Remove(thingDef);
-                thingDef.thingCategories.Remove(rottenCategoryDef);
-                rottenCategoryDef.ClearCachedData();
-            }
-
-            MeatRawCategory.ClearCachedData();
-            MeatBadCategory.ClearCachedData();
-            MeatRawCategory.ResolveReferences();
-            MeatBadCategory.ResolveReferences();
-            Log.Message($"[BadMeatCategory]: Moved {thingsToMove.Count} meat to the Bad Meat-category");
+            Log.ErrorOnce("[BadMeatCategory]: Could not find the MeatRaw-category. Will not sort bad meat.",
+                "MeatRawCategory".GetHashCode());
+            return;
         }
+
+        var MeatBadCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatBad");
+        if (MeatBadCategory == null)
+        {
+            Log.ErrorOnce("[BadMeatCategory]: Could not find the MeatBad-category. Will not sort bad meat.",
+                "MeatBadCategory".GetHashCode());
+            return;
+        }
+
+        var extraThingDefs = new List<string>
+        {
+            "Meat_Chaocow",
+            "MeatRotten"
+        };
+        var thingsToMove = new List<ThingDef>();
+        foreach (var descendantThingDef in MeatRawCategory.DescendantThingDefs)
+        {
+            if (FoodUtility.GetMeatSourceCategory(descendantThingDef) == MeatSourceCategory.Humanlike)
+            {
+                thingsToMove.Add(descendantThingDef);
+                continue;
+            }
+
+            if (descendantThingDef.ingestible.sourceDef?.race != null &&
+                descendantThingDef.ingestible.sourceDef.race.FleshType == FleshTypeDefOf.Insectoid)
+            {
+                thingsToMove.Add(descendantThingDef);
+                continue;
+            }
+
+            if (extraThingDefs.Contains(descendantThingDef.defName))
+            {
+                thingsToMove.Add(descendantThingDef);
+            }
+        }
+
+        foreach (var thingDef in thingsToMove)
+        {
+            MeatRawCategory.childThingDefs.Remove(thingDef);
+            thingDef.thingCategories.Remove(MeatRawCategory);
+            MeatBadCategory.childThingDefs.Add(thingDef);
+            thingDef.thingCategories.Add(MeatBadCategory);
+            if (thingDef.defName != "MeatRotten")
+            {
+                continue;
+            }
+
+            // Support for Rotted Meat-mod
+            var rottenCategoryDef = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("MeatRawRotten");
+            if (rottenCategoryDef == null)
+            {
+                continue;
+            }
+
+            rottenCategoryDef.childThingDefs.Remove(thingDef);
+            thingDef.thingCategories.Remove(rottenCategoryDef);
+            rottenCategoryDef.ClearCachedData();
+        }
+
+        MeatRawCategory.ClearCachedData();
+        MeatBadCategory.ClearCachedData();
+        MeatRawCategory.ResolveReferences();
+        MeatBadCategory.ResolveReferences();
+        Log.Message($"[BadMeatCategory]: Moved {thingsToMove.Count} meat to the Bad Meat-category");
     }
 }
